45f7f74372da4ae07aa62a7f16597d43454bfaf0
ricardoboterorios <ricardoboterorios@yahoo.es>
2015-06-30 23:06:29 -0500

    Primera Pregunta

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 
+var quizController = require('../controllers/quiz_controller');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'QUIZ' });
 });
+
+// Definición de rutas de /quizes
+router.get('/quizes/question', quizController.question);
+router.get('/quizes/answer', quizController.answer);
 
 
 module.exports = router;
