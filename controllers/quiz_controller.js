50dd314020fccd25dbde1188e633b5df9b07a690
ricardoboterorios <ricardoboterorios@yahoo.es>
2015-07-14 16:22:27 -0500

    sequelize.js y SQLite

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,13 +1,20 @@
-exports.question = function(req, res){
-	 res.render('quizes/question', {pregunta: 'Capital de Italia'});
+var models = require('../models/models.js');
 
+exports.question = function(req, res){
+models.Quiz.findAll().success(
+    function(quiz) {
+      res.render('quizes/question', { pregunta: quiz[0].pregunta})
+    })
 };
 
 exports.answer = function(req, res){
-	 if(req.query.respuesta==='Roma'){
+	models.Quiz.findAll().success(
+	function(quiz) {
+	 if(req.query.respuesta===quiz[0].respuesta){
 	 	res.render('quizes/answer',{respuesta: 'Correcto'});
 	 }
 	 else{
 	 	res.render('quizes/answer',{respuesta: 'Incorrecto'});
 	 }
+	})
 };
