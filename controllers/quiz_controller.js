ee2f00a53b7002c692cb2412c6c74827ae6b2187
ricardoboterorios <ricardoboterorios@yahoo.es>
2015-07-19 15:16:43 -0500

    Despliegue DB en Heroku

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,20 +1,28 @@
 var models = require('../models/models.js');
 
 exports.question = function(req, res){
-models.Quiz.findAll().success(
-    function(quiz) {
-      res.render('quizes/question', { pregunta: quiz[0].pregunta})
+models.Quiz.find(req.params.quizId).then(function(quiz) {
+      res.render('quizes/question', { pregunta: quiz.pregunta})
+      //console.log('quiz.pregunta');
+      //console.log(quiz.pregunta);
+     // console.log("quiz.id " + quiz[{pregunta}]);
+
     })
 };
 
 exports.answer = function(req, res){
-	models.Quiz.findAll().success(
+	models.Quiz.find(req.params.quizId).then(
 	function(quiz) {
-	 if(req.query.respuesta===quiz[0].respuesta){
+	 if(req.query.respuesta===quiz.respuesta){
 	 	res.render('quizes/answer',{respuesta: 'Correcto'});
 	 }
 	 else{
 	 	res.render('quizes/answer',{respuesta: 'Incorrecto'});
+	 	//console.log('req.query.respuesta' +  req.query.respuesta);
+	 	//console.log('quiz' + quiz);
+		//console.log(req.query.respuesta);
+		//console.log('quiz.respuesta');
+		//console.log(quiz.respuesta);
 	 }
 	})
 };
